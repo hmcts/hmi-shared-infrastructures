ab784aa863811262bac8b8956c17ad34bbe01a7d
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-03-24 19:57:17 +0000

    Added kv

--- main.tf
+++ main.tf
@@ -1,51 +1,9 @@
 locals {
   prefix = "${var.product}-sharedinfra"
-  containers = [{
-    name        = "rota"
-    access_type = "private"
-  }]
-  storage_account_name = "${var.product}sa${var.env}"
   resource_group_name  = "${local.prefix}-${var.env}-rg"
 }
 
 # Needs to be a resource when current repo is deleted.
 data "azurerm_resource_group" "rg" {
-  name     = local.resource_group_name
-}
-
-data "azurerm_user_assigned_identity" "hmi-identity" {
- name                = "${var.product}-${var.env}-mi"
- resource_group_name = "managed-identities-${var.env}-rg"
-}
-
-#tfsec:ignore:azure-storage-default-action-deny
-module "sa" {
-  source = "git@github.com:hmcts/cnp-module-storage-account?ref=master"
-
-  env = var.env
-
-  storage_account_name = local.storage_account_name
-  common_tags          = var.common_tags
-
-  default_action = "Allow"
-
-  resource_group_name = data.azurerm_resource_group.rg.name
-  location            = var.location
-
-  account_tier                    = var.sa_account_tier
-  account_kind                    = var.sa_account_kind
-  account_replication_type        = var.sa_account_replication_type
-  access_tier                     = var.sa_access_tier
-
-  enable_data_protection = true
-
-  team_name    = var.team_name
-  team_contact = var.team_contact
-
-  containers = local.containers
-
-  managed_identity_object_id = data.azurerm_user_assigned_identity.hmi-identity.principal_id
-  role_assignments = [
-    "Storage Blob Data Reader"
-  ]
+  name = local.resource_group_name
 }
--- variables.tf
+++ variables.tf
@@ -31,3 +31,12 @@ variable "sa_account_replication_type" {
   type    = string
   default = "RAGRS"
 }
+variable "active_directory_group" {
+  type        = string
+  description = "Active Directory Group Name"
+  default     = "DTS SDS Developers"
+}
+variable "jenkins_identity_object_id" {
+  description = "Objectid for jenkins managed identity"
+  default     = "7ef3b6ce-3974-41ab-8512-c3ef4bb8ae01"
+}
