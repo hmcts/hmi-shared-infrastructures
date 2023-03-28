8f1784cfc731236d2f4daeeb3c16a6249db738cc
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-03-28 13:29:57 +0100

    Added kv secrets

--- key-vaults.tf
+++ key-vaults.tf
@@ -1,5 +1,6 @@
 locals {
   key_vault_name = "${var.product}-kv-${var.env}"
+  secret_expiry = "2024-03-01T01:00:00Z"
 }
 
 module "kv_hmi" {
@@ -12,4 +13,24 @@ module "kv_hmi" {
   product_group_name      = var.active_directory_group
   common_tags             = var.common_tags
   create_managed_identity = false
+}
+
+module "keyvault_secrets" {
+  source = "./modules/kv_secrets"
+
+  key_vault_id = module.kv_hmi.key_vault_id
+  tags         = var.common_tags
+  secrets = [
+    {
+      name            = "sa-connection-string"
+      value           = module.kv_hmi.storageaccount_primary_connection_string
+      tags            = {}
+      content_type    = ""
+      expiration_date = local.secret_expiry
+    }
+  ]
+
+  depends_on = [
+    module.kv_hmi
+  ]
 }
