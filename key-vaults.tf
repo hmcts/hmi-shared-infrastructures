e938c73835bb823eb3825fc243a185a5b3bd1ce5
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-03-28 14:04:50 +0100

    Added access

--- key-vaults.tf
+++ key-vaults.tf
@@ -3,6 +3,32 @@ locals {
   secret_expiry = "2024-03-01T01:00:00Z"
 }
 
+resource "azurerm_key_vault_access_policy" "github_access" {
+  key_vault_id = module.kv.key_vault_id
+
+  object_id = var.GITHUB_RUNNER_OBJECT_ID
+  tenant_id = data.azurerm_client_config.current.tenant_id
+
+  certificate_permissions = []
+  key_permissions         = []
+  secret_permissions = [
+    "Get",
+  ]
+}
+
+resource "azurerm_key_vault_access_policy" "cft_jenkins_access" {
+  key_vault_id = module.kv.key_vault_id
+
+  object_id = "ca6d5085-485a-417d-8480-c3cefa29df31"
+  tenant_id = data.azurerm_client_config.current.tenant_id
+
+  certificate_permissions = []
+  key_permissions         = []
+  secret_permissions = [
+    "Get",
+  ]
+}
+
 module "kv_hmi" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   name                    = local.key_vault_name
