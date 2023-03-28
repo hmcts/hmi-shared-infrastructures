3d89b74a2a4478ead75ba1727160c773831f647d
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-03-28 14:06:28 +0100

    Added access

--- key-vaults.tf
+++ key-vaults.tf
@@ -4,7 +4,7 @@ locals {
 }
 
 resource "azurerm_key_vault_access_policy" "github_access" {
-  key_vault_id = module.kv.key_vault_id
+  key_vault_id = module.kv_hmi.key_vault_id
 
   object_id = var.GITHUB_RUNNER_OBJECT_ID
   tenant_id = data.azurerm_client_config.current.tenant_id
@@ -17,7 +17,7 @@ resource "azurerm_key_vault_access_policy" "github_access" {
 }
 
 resource "azurerm_key_vault_access_policy" "cft_jenkins_access" {
-  key_vault_id = module.kv.key_vault_id
+  key_vault_id = module.kv_hmi.key_vault_id
 
   object_id = "ca6d5085-485a-417d-8480-c3cefa29df31"
   tenant_id = data.azurerm_client_config.current.tenant_id
