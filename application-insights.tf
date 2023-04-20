24e4587b9a8172107678e2a81d00c97f3c41279c
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-04-20 15:52:36 +0100

    Added app insights infra

---
+++ application-insights.tf
@@ -0,0 +1,7 @@
+resource "azurerm_application_insights" "rota_dtu_app_insights" {
+  name                = "${var.product}-rota-dtu-${var.env}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  application_type    = "java"
+  tags                = var.common_tags
+}
