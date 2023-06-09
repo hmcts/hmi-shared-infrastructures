89a06a32cc716a01ab1a9c5539c1209ab1fdda91
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-06-09 13:14:23 +0100

    updated product var

--- automation-account.tf
+++ automation-account.tf
@@ -1,5 +1,5 @@
 resource "azurerm_automation_account" "automation_account" {
-  name                = "${local.product}-${var.env}-aa"
+  name                = "${var.product}-${var.env}-aa"
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   sku_name            = var.automation_account_sku_name
