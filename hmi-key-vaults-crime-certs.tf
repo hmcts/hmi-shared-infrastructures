1465915def3afe5dd62c1b1a725f62a4a19de9d6
joshblackmoor <joshblackmoor@users.noreply.github.com>
2023-09-20 15:33:59 +0000

    Automated Change

--- hmi-key-vaults-crime-certs.tf
+++ hmi-key-vaults-crime-certs.tf
@@ -1,36 +1,36 @@
 locals {
-    crime_cert_kv_name = var.env == "prod" ? "hmi-crime-prod" : "hmi-crime-non-prod"
+  crime_cert_kv_name = var.env == "prod" ? "hmi-crime-prod" : "hmi-crime-non-prod"
 }
 
 data "azurerm_key_vault_certificate" "crime_bootstrap_cert" {
-    name         = local.crime_cert_kv_name
-    key_vault_id = data.azurerm_key_vault.bootstrap_kv.id
+  name         = local.crime_cert_kv_name
+  key_vault_id = data.azurerm_key_vault.bootstrap_kv.id
 }
 
 resource "azurerm_key_vault_certificate" "crime_hmi_kv_cert" {
-    name         = data.azurerm_key_vault_certificate.crime_bootstrap_cert.name
-    key_vault_id = module.kv_hmi.key_vault_id
+  name         = data.azurerm_key_vault_certificate.crime_bootstrap_cert.name
+  key_vault_id = module.kv_hmi.key_vault_id
 
-    certificate {
-        contents = data.azurerm_key_vault_certificate.crime_bootstrap_cert.value
+  certificate {
+    contents = data.azurerm_key_vault_certificate.crime_bootstrap_cert.value
+  }
+
+  certificate_policy {
+    issuer_parameters {
+      name = "self"
     }
 
-    certificate_policy {
-        issuer_parameters {
-            name = "self"
-        }
-
-        key_properties {
-            exportable = true
-            key_size   = data.azurerm_key_vault_certificate.crime_bootstrap_cert.certificate_policy.0.key_properties.0.key_size
-            key_type   = data.azurerm_key_vault_certificate.crime_bootstrap_cert.certificate_policy.0.key_properties.0.key_type
-            reuse_key  = true
-        }
-
-        secret_properties {
-            content_type = "application/x-pkcs12"
-        }
-
+    key_properties {
+      exportable = true
+      key_size   = data.azurerm_key_vault_certificate.crime_bootstrap_cert.certificate_policy.0.key_properties.0.key_size
+      key_type   = data.azurerm_key_vault_certificate.crime_bootstrap_cert.certificate_policy.0.key_properties.0.key_type
+      reuse_key  = true
     }
-    tags = var.common_tags
+
+    secret_properties {
+      content_type = "application/x-pkcs12"
+    }
+
+  }
+  tags = var.common_tags
 }
