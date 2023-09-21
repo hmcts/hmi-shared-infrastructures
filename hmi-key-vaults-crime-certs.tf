898251dae8643c5bf6effa7bb1808b1c81c7a36f
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-09-21 09:24:45 +0100

    Changes

--- hmi-key-vaults-crime-certs.tf
+++ hmi-key-vaults-crime-certs.tf
@@ -1,36 +1,24 @@
 locals {
-  crime_cert_kv_name = var.env == "prod" ? "hmi-crime-prod" : "hmi-crime-non-prod"
+    crime_cert_kv_name = "hmi-crime-cert-${var.env}"
+    crime_cert_kv_password_name = "hmi-crime-cert-password"
 }
 
 data "azurerm_key_vault_certificate" "crime_bootstrap_cert" {
-  name         = local.crime_cert_kv_name
-  key_vault_id = data.azurerm_key_vault.bootstrap_kv.id
+    name         = local.crime_cert_kv_name
+    key_vault_id = data.azurerm_key_vault.bootstrap_kv.id
 }
 
-resource "azurerm_key_vault_certificate" "crime_hmi_kv_cert" {
-  name         = data.azurerm_key_vault_certificate.crime_bootstrap_cert.name
-  key_vault_id = module.kv_hmi.key_vault_id
+data "azurerm_key_vault_secret" "crime_bootstrap_cert_password" {
+    name         = local.crime_cert_kv_password_name
+    key_vault_id = module.kv_hmi.key_vault_id
+}
 
-  certificate {
-    contents = data.azurerm_key_vault_certificate.crime_bootstrap_cert.value
-  }
+resource "azurerm_key_vault_certificate" "import_crime_cert" {
+    name         = data.azurerm_key_vault_certificate.crime_bootstrap_cert.name
+    key_vault_id = module.kv_hmi.key_vault_id
 
-  certificate_policy {
-    issuer_parameters {
-      name = "self"
+    certificate {
+        certificate_contents = data.azurerm_key_vault_certificate.crime_bootstrap_cert.certificate_contents
+        password              = data.azurerm_key_vault_secret.crime_bootstrap_cert_password.value
     }
-
-    key_properties {
-      exportable = true
-      key_size   = data.azurerm_key_vault_certificate.crime_bootstrap_cert.certificate_policy.0.key_properties.0.key_size
-      key_type   = data.azurerm_key_vault_certificate.crime_bootstrap_cert.certificate_policy.0.key_properties.0.key_type
-      reuse_key  = true
-    }
-
-    secret_properties {
-      content_type = "application/x-pkcs12"
-    }
-
-  }
-  tags = var.common_tags
 }
