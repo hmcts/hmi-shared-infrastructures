bd74df35d194e40382d875ddabe710cd1d7002f4
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-11-29 10:30:43 +0000

    temp disable automation account

--- automation-account.tf
+++ automation-account.tf
@@ -1,16 +1,16 @@
-resource "azurerm_automation_account" "automation_account" {
-  name                = "${var.product}-${var.env}-aa"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  sku_name            = var.automation_account_sku_name
+# resource "azurerm_automation_account" "automation_account" {
+#   name                = "${var.product}-${var.env}-aa"
+#   location            = azurerm_resource_group.rg.location
+#   resource_group_name = azurerm_resource_group.rg.name
+#   sku_name            = var.automation_account_sku_name
 
-  identity {
-    type = "SystemAssigned, UserAssigned"
-    identity_ids = [
-      data.azurerm_user_assigned_identity.hmi.id,
-      var.jenkins_mi_resource_id
-    ]
-  }
+#   identity {
+#     type = "SystemAssigned, UserAssigned"
+#     identity_ids = [
+#       data.azurerm_user_assigned_identity.hmi.id,
+#       var.jenkins_mi_resource_id
+#     ]
+#   }
 
-  tags = var.common_tags
-}
+#   tags = var.common_tags
+# }
