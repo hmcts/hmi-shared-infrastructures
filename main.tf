1eabb86340dc13aac95cd729bd033a3fcd9c6e47
Josh Blackmoor <joshblackmoor01@icloud.com>
2023-11-29 14:41:36 +0000

    Removed old MI data source as not needed

--- main.tf
+++ main.tf
@@ -29,11 +29,6 @@ resource "azurerm_resource_group" "rg" {
 
 data "azurerm_client_config" "current" {}
 
-# data "azurerm_user_assigned_identity" "hmi" {
-#   name                = "hmi-${var.env}-mi"
-#   resource_group_name = "managed-identities-${var.env}-rg"
-# }
-
 resource "azurerm_user_assigned_identity" "hmi-sds-mi" {
   name                = "hmi-sds-${var.env}-mi"
   resource_group_name = "managed-identities-${var.env}-rg"
