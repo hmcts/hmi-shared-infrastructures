cc417df779120dba4ebf79c76580ece0119c0abc
ChrisS1512 <87066931+ChrisS1512@users.noreply.github.com>
2025-04-10 10:07:58 +0100

    PUB-2862 - Dependency Updates

--- state.tf
+++ state.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.20.0"
+      version = "4.26.0"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "~> 2.2.0"
+      version = "~> 2.3.0"
     }
   }
 }
