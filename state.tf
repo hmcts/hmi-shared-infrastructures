4a6a3c2b4820b4023d62b7ac8fbea420697f645d
KianKwa <kian.kwa@justice.gov.uk>
2026-03-20 15:34:15 +0000

    Dependency updates

--- state.tf
+++ state.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.51.0"
+      version = "4.65.0"
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "~> 2.5.0"
+      version = "~> 2.8.0"
     }
   }
 }
