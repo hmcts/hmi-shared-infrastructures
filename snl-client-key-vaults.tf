97241a0a728dd363afdd79dea8a6c6b1ac93761f
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-09-14 16:22:07 +0100

    use jenkins_object_id

--- snl-client-key-vaults.tf
+++ snl-client-key-vaults.tf
@@ -9,7 +9,7 @@ module "kv_snl" {
   name                    = local.snl_key_vault_name
   product                 = var.product
   env                     = var.env
-  object_id               = var.jenkins_identity_object_id
+  jenkins_object_id       = var.jenkins_identity_object_id
   resource_group_name     = azurerm_resource_group.rg.name
   product_group_name      = var.active_directory_group
   common_tags             = var.common_tags
